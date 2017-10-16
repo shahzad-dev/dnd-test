af754898eae94b574f3838f7e03577ae22679de0
Shahzad <shahzadt.dev@gmail.com>
2017-10-16 17:09:59 -0400

    Added few more examples to test dnd

--- js/components/App.js
+++ js/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from 'react';;
 import Relay from 'react-relay';
 import HobbiesList from './units/Hobbies';
+import Example1 from './Example1';
+import Example2 from './Example2';
+import DustbinSingleTarget from './example3';
 
 class App extends React.Component {
-  render() {
-    return (<div><HobbiesList Viewer={this.props.Viewer} /></div>)
-  }
+    render() {
+        return (
+            <div style={{width: "50%", height: 500, border: "1px solid", padding: 10}}>
+                {/*<Example1 />*/}
+                {/*<Example2 />*/}
+                {/*<DustbinSingleTarget />*/}
+            </div>
+    )}
 }
 
 export default Relay.createContainer(App, {
